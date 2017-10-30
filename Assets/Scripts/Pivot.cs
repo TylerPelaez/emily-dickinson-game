ebb14b2f7bca06446b158a2c13725e91dd359bed
DefectiveMan <pelaet@rpi.edu>
2017-10-30 00:21:55 -0400

    Moving the sun around teh player, using character controller for testing

---
+++ Assets/Scripts/Pivot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour {
+
+	Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.Find ("FPSController").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
+	}
+
+	public void Rotate(float amt) {
+		transform.RotateAround(transform.position, transform.forward, amt);
+	}
+}
