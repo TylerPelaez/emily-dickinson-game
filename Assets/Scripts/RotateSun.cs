07a44f9e8d77e389f5e88fcdf45d68bbb296b4ba
DefectiveMan <pelaet@rpi.edu>
2017-10-30 00:21:55 -0400

    Moving the sun around teh player, using character controller for testing

---
+++ Assets/Scripts/RotateSun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSun : MonoBehaviour {
+
+	Pivot sunObjectPivot;
+	const float SUN_ROTATE_SPEED = 30f;
+
+	// Use this for initialization
+	void Start () {
+		sunObjectPivot = GameObject.Find ("Sun Pivot").GetComponent<Pivot> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1")) {
+			if(sunObjectPivot != null) {
+				sunObjectPivot.Rotate(Time.deltaTime * SUN_ROTATE_SPEED);
+			}
+		} else if (Input.GetButton("Fire2")) {
+			if(sunObjectPivot != null) {
+				sunObjectPivot.Rotate(- Time.deltaTime * SUN_ROTATE_SPEED);
+			}
+		}
+	}
+}
