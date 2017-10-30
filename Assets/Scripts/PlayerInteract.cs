2854a5127d00df9823b6e1b298e880e683582366
DefectiveMan <pelaet@rpi.edu>
2017-10-30 19:22:14 -0400

    Pick up and put down objects with F. Made the player a prefab. Merged Sun rotation with player movement

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour {
 
+	Transform cameraTransform;
 	GameObject heldObject;
+	const float HOLD_DISTANCE = 2.0f;
+	const float INTERACT_DISTANCE = 3.0f;
+
 
 	// Use this for initialization
 	void Start () {
 		heldObject = null;
+		cameraTransform = gameObject.GetComponentInChildren<Camera> ().gameObject.transform;
 	}
 
 	void centerObjectInCamera() {
 		if (heldObject != null) {
-			Vector3 posVec = new Vector3 (transform.position.x + transform.forward.x, transform.position.y + transform.forward.y, transform.position.z + transform.forward.z);
+			Vector3 posVec = new Vector3 (cameraTransform.position.x + (cameraTransform.forward.x * HOLD_DISTANCE), cameraTransform.position.y + (cameraTransform.forward.y * HOLD_DISTANCE), cameraTransform.position.z + (cameraTransform.forward.z * HOLD_DISTANCE));
 			heldObject.transform.position = posVec;
+			heldObject.transform.eulerAngles = new Vector3 (0f, cameraTransform.rotation.eulerAngles.y, 0f);
 		}
 	}
 	
@@ -24,20 +30,25 @@ public class PlayerInteract : MonoBehaviour {
 			if (heldObject == null) {
 				// Try to pick up an object
 				RaycastHit hitInfo;
-				if (Physics.Raycast (transform.position, transform.forward, out hitInfo, 1.0f, LayerMask.GetMask("Player"))) {
-					Debug.Log ("Fire");
+				// Debug.Log ("Fire");
+				// Debug.DrawRay (transform.position, transform.forward * INTERACT_DISTANCE, Color.green, 5.0f);
+				if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hitInfo, INTERACT_DISTANCE, LayerMask.GetMask("Default"))) {
+					
 					Debug.Log (hitInfo.collider.gameObject.name);
 
 					if (hitInfo.collider.gameObject.GetComponent<Pickupable> ()) {
-						GameObject other = hitInfo.collider.gameObject;
-						heldObject = Instantiate (other, gameObject.transform);
-						Destroy (hitInfo.collider.gameObject);
-						centerObjectInCamera ();
+						heldObject = hitInfo.collider.gameObject;
+						heldObject.GetComponent<Rigidbody> ().useGravity = false;
+						heldObject.GetComponent<Rigidbody> ().detectCollisions = false;
 					}
 				}
 			} else {
 				// Drop Object
+				heldObject.GetComponent<Rigidbody> ().useGravity = true;
+				heldObject.GetComponent<Rigidbody> ().detectCollisions = true;
+				heldObject = null;
 			}
 		}
+		centerObjectInCamera ();
 	}
 }
