4d829aed508fa02589b7b33455ecccdb7c89da84
DefectiveMan <pelaet@rpi.edu>
2017-11-01 13:11:08 -0400

    Objects float in the right spot now

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -18,7 +18,15 @@ public class PlayerInteract : MonoBehaviour {
 
 	void centerObjectInCamera() {
 		if (heldObject != null) {
-			Vector3 posVec = new Vector3 (cameraTransform.position.x + (cameraTransform.forward.x * HOLD_DISTANCE), cameraTransform.position.y + (cameraTransform.forward.y * HOLD_DISTANCE), cameraTransform.position.z + (cameraTransform.forward.z * HOLD_DISTANCE));
+			RaycastHit hit;
+			Vector3 posVec;
+			Vector3 objectExtent = heldObject.GetComponent<BoxCollider> ().bounds.extents;
+
+			if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hit, HOLD_DISTANCE, LayerMask.GetMask ("Terrain"))) {
+				posVec = new Vector3 (hit.point.x - (cameraTransform.forward.x * objectExtent.x), hit.point.y - (cameraTransform.forward.y * objectExtent.y), hit.point.z - (cameraTransform.forward.z * objectExtent.z));
+			} else {
+				posVec = new Vector3 (cameraTransform.position.x + (cameraTransform.forward.x * HOLD_DISTANCE), cameraTransform.position.y + (cameraTransform.forward.y * HOLD_DISTANCE), cameraTransform.position.z + (cameraTransform.forward.z * HOLD_DISTANCE));
+			}
 			heldObject.transform.position = posVec;
 			heldObject.transform.eulerAngles = new Vector3 (0f, cameraTransform.rotation.eulerAngles.y, 0f);
 		}
@@ -32,20 +40,18 @@ public class PlayerInteract : MonoBehaviour {
 				RaycastHit hitInfo;
 				// Debug.Log ("Fire");
 				// Debug.DrawRay (transform.position, transform.forward * INTERACT_DISTANCE, Color.green, 5.0f);
-				if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hitInfo, INTERACT_DISTANCE, LayerMask.GetMask("Default"))) {
+				if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hitInfo, INTERACT_DISTANCE, LayerMask.GetMask("Pickupable"))) {
 					
 					Debug.Log (hitInfo.collider.gameObject.name);
 
 					if (hitInfo.collider.gameObject.GetComponent<Pickupable> ()) {
 						heldObject = hitInfo.collider.gameObject;
 						heldObject.GetComponent<Rigidbody> ().useGravity = false;
-						heldObject.GetComponent<Rigidbody> ().detectCollisions = false;
 					}
 				}
 			} else {
 				// Drop Object
 				heldObject.GetComponent<Rigidbody> ().useGravity = true;
-				heldObject.GetComponent<Rigidbody> ().detectCollisions = true;
 				heldObject = null;
 			}
 		}
