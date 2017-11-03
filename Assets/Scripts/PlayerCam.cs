336b5815d84b47914fee1043dc16e2b4663853a4
Millmoss <omelliff@yahoo.com>
2017-11-03 14:35:06 -0400

    Made camera smoother, merged Annie's and Tyler's object pickup code with some success.

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -5,18 +5,131 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerCam : MonoBehaviour
 {
-    public MouseLook camLook;
-    
-	void Start ()
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -47F;
+    public float MaximumX = 47F;
+    public bool smooth;
+    public float smoothTime = 5f;
+    public bool lockCursor = true;
+    public float yRot;
+    public float xRot;
+    public float yMov;
+    public float xMov;
+
+
+    private Quaternion m_CharacterTargetRot;
+    private Quaternion m_CameraTargetRot;
+    private bool m_cursorIsLocked = true;
+
+    void Start ()
     {
-        camLook = new MouseLook();
-        camLook.Init(gameObject.transform, Camera.main.transform);
+        Init(gameObject.transform, Camera.main.transform);
 	}
 	
 	void Update ()
     {
-        camLook.LookRotation(gameObject.transform, Camera.main.transform);
+        yMov = CrossPlatformInputManager.GetAxis("Mouse Y");
+        xMov = CrossPlatformInputManager.GetAxis("Mouse X");
     }
+
+    void FixedUpdate()
+    {
+        LookRotation(gameObject.transform, Camera.main.transform);
+    }
+
+    public void Init(Transform character, Transform camera)
+    {
+        m_CharacterTargetRot = character.localRotation;
+        m_CameraTargetRot = camera.localRotation;
+        smooth = false;
+    }
+
+
+    public void LookRotation(Transform character, Transform camera)
+    {
+        yRot = xMov * XSensitivity;
+        xRot = yMov * YSensitivity;
+
+        m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        if (clampVerticalRotation)
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+        if (smooth)
+        {
+            character.localRotation = Quaternion.Slerp(character.localRotation, m_CharacterTargetRot,
+                smoothTime * Time.deltaTime);
+            camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                smoothTime * Time.deltaTime);
+        }
+        else
+        {
+            character.localRotation = m_CharacterTargetRot;
+            camera.localRotation = m_CameraTargetRot;
+        }
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
 }
 
 public class MouseLook
--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -11,11 +11,13 @@ public class PlayerInteract : MonoBehaviour {
 	const float INTERACT_DISTANCE = 3.0f;
 	Text pickupTextObject;
 	RigidbodyConstraints heldObjectConstraints;
+    Vector3 objectExtent;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		heldObject = null;
+        objectExtent = Vector3.zero;
 		cameraTransform = gameObject.GetComponentInChildren<Camera> ().gameObject.transform;
 
 		pickupTextObject = GameObject.Find ("PickupText").GetComponent<Text>();
@@ -25,7 +27,7 @@ public class PlayerInteract : MonoBehaviour {
 		if (heldObject != null) {
 			RaycastHit hit;
 			Vector3 posVec;
-			Vector3 objectExtent = heldObject.GetComponent<Collider> ().bounds.size;
+			objectExtent = heldObject.GetComponent<Collider> ().bounds.size;
 
 			if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hit, HOLD_DISTANCE, LayerMask.GetMask ("Terrain"))) {
 				posVec = new Vector3 (hit.point.x - (cameraTransform.forward.x * objectExtent.x + 0.1f), hit.point.y - (cameraTransform.forward.y * objectExtent.y+ 0.1f), hit.point.z - (cameraTransform.forward.z * objectExtent.z + 0.1f));
@@ -33,14 +35,61 @@ public class PlayerInteract : MonoBehaviour {
 				posVec = new Vector3 (cameraTransform.position.x + (cameraTransform.forward.x * HOLD_DISTANCE), cameraTransform.position.y + (cameraTransform.forward.y * HOLD_DISTANCE), cameraTransform.position.z + (cameraTransform.forward.z * HOLD_DISTANCE));
 				Debug.Log (posVec);
 			}
-			heldObject.transform.position = posVec;
-			heldObject.transform.eulerAngles = new Vector3 (0f, cameraTransform.rotation.eulerAngles.y, 0f);
+            heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, posVec, .1f);
+            heldObject.transform.eulerAngles = Vector3.Lerp(heldObject.transform.eulerAngles, new Vector3(0f, cameraTransform.rotation.eulerAngles.y, 0f), .1f);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (heldObject == null) {
+        if (heldObject == null)
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hitInfo, INTERACT_DISTANCE, LayerMask.GetMask("Pickupable")))
+            {
+                if (Input.GetButtonDown("Interact"))
+                {
+                    Debug.Log(hitInfo.collider.gameObject.name);
+
+                    if (hitInfo.collider.gameObject.GetComponent<Pickupable>())
+                    {
+                        heldObject = hitInfo.collider.gameObject;
+                        heldObject.GetComponent<Rigidbody>().useGravity = false;
+                        heldObject.GetComponent<Default_Obj_Behavior>().held = true;
+                        //heldObject.GetComponent<Rigidbody>().detectCollisions = false;
+                        heldObjectConstraints = heldObject.GetComponent<Rigidbody>().constraints;
+                        heldObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+                    }
+                    else
+                    {
+                        pickupTextObject.enabled = true;
+                        pickupTextObject.text = "Click to Pick up.";
+                    }
+                }
+                else
+                {
+                    pickupTextObject.enabled = true;
+                    pickupTextObject.text = "Click to Pick up.";
+                }
+            }
+        }
+        else if (Input.GetButtonDown("Interact"))
+        {
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
+            //heldObject.GetComponent<Rigidbody>().detectCollisions = true;
+            heldObject.GetComponent<Rigidbody>().constraints = heldObjectConstraints;
+            // Drop Object
+            heldObject.GetComponent<Default_Obj_Behavior>().SnapTo();
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
+            heldObject.GetComponent<Default_Obj_Behavior>().held = false;
+            heldObject = null;
+        }
+        else
+        {
+            pickupTextObject.text = "";
+            pickupTextObject.enabled = false;
+        }
+        /*if (heldObject == null) {
 			RaycastHit hitInfo;
 			if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hitInfo, INTERACT_DISTANCE, LayerMask.GetMask ("Pickupable"))) {
 				if (Input.GetButtonDown ("Interact")) {
@@ -71,10 +120,10 @@ public class PlayerInteract : MonoBehaviour {
 			
 			pickupTextObject.text = "";
 			pickupTextObject.enabled = false;
-		}
-	}
+		}*/
+        centerObjectInCamera();
+    }
 
-	void FixedUpdate() {
-		centerObjectInCamera ();
+    void FixedUpdate() {
 	}
 }
