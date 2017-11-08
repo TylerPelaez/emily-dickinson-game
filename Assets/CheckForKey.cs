09ed64b4ae00ca5636785aa88ac5ab895928d76f
DefectiveMan <pelaet@rpi.edu>
2017-11-08 11:40:17 -0500

    Explosive Force to keyed door

---
+++ Assets/CheckForKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForKey : MonoBehaviour {
+
+	public GameObject Wall;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "key") {
+			Debug.Log ("Hi, Key!");
+			Wall.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+			Wall.GetComponent<Rigidbody> ().AddExplosionForce (10f, other.gameObject.transform.position, 3f);
+
+		}
+	}
+}
