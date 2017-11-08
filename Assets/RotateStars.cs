f3f158a5b68e6bad5e6c0609b9307aad4cf7d8d1
NoahZr <n.zucker1@gmail.com>
2017-11-08 10:18:34 -0500

    fixed stars so they don't appear in daytime. added more menu stuff

--- Assets/RotateStars.cs
+++ Assets/RotateStars.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class RotateStars : MonoBehaviour {
     public Vector3 rotationSpeed;
+   public Playback sky;
+    Material starMat;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        starMat = transform.GetChild(1).GetComponent<Renderer>().material;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         transform.rotation *= Quaternion.Euler( rotationSpeed);
-	}
+        if(sky.progress> .7f||sky.progress<.3)
+        {
+            starMat.SetColor("_TintColor", Color.Lerp(starMat.GetColor("_TintColor"), new Color(183, 183, 183, 0), 8 * Time.deltaTime));
+        }
+        else
+        {
+            starMat.SetColor("_TintColor", Color.Lerp(starMat.GetColor("_TintColor"), new Color(183, 183, 183, 1), 8 * Time.deltaTime));
+        }
+    }
 }
