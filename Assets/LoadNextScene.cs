dfae4a104b4943fa40b319dece4f28ab300116d1
DefectiveMan <pelaet@rpi.edu>
2017-11-08 09:59:56 -0500

    Scene Transitions

---
+++ Assets/LoadNextScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision coll) {
+		if (coll.other.gameObject.name == "Player") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
