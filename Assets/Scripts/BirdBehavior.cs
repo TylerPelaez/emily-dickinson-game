b53b2aa0086746714f885efa45f1bb754b9639c8
DefectiveMan <pelaet@rpi.edu>
2017-11-07 21:16:52 -0500

    Bird Levels

---
+++ Assets/Scripts/BirdBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdBehavior : MonoBehaviour {
+
+	Vector3 fencePosition;
+	private int row;
+	private int col;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setFencePosition(Vector3 pos) {
+		fencePosition = new Vector3 (pos.x, pos.y, pos.z);
+	}
+
+	public void setRowAndColumn(int row_, int column_) {
+		row = row_;
+		col = column_;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setToFencePosition() {
+		transform.position = new Vector3 (fencePosition.x, fencePosition.y, fencePosition.z);
+	}
+}
---
+++ Assets/Scripts/FenceBehavior.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class RotationRangeObject {
+	private float beginRotation;
+	private float endRotation;
+
+	public RotationRangeObject(float begin, float end) {
+		beginRotation = begin;
+		endRotation = end;
+	}
+
+	public float getBeginRotation() {
+		return beginRotation;
+	}
+
+	public float getEndRotation() {
+		return endRotation;
+	}
+
+	public bool inRange(float rot) {
+		if (beginRotation > endRotation) {
+			return rot >= beginRotation || rot <= endRotation;
+		} else {
+			return rot >= beginRotation && rot <= endRotation;
+		}
+	}
+
+}
+
+public class FenceBehavior : MonoBehaviour {
+
+	public GameObject sunPivotObject;
+	public GameObject birdPrefab;
+
+	private RotationRangeObject[] ranges;
+	private GameObject[] birds;
+
+	private int[,] indices = new int[3,5] { { 0, 1, 2, 3, 4 }, { 5, 6, 7, 8, 9 }, { 10, 11, 12, 13, 14 } };
+
+	private int current_range;
+
+
+	// Use this for initialization
+	void Start () {
+		current_range = -1;
+		ranges = new RotationRangeObject[3];
+		ranges[0] = new RotationRangeObject (320, 340);
+		ranges[1] = new RotationRangeObject (290, 310);
+		ranges[2] = new RotationRangeObject (350, 10);
+
+		birds = new GameObject[15];
+
+		for (int i = 0; i < 15; i++) {
+			birds [i] = Instantiate (birdPrefab, transform.position , transform.rotation);
+
+			birds [i].GetComponent<BirdBehavior>().setFencePosition(new Vector3(transform.position.x - 1.5f + ((i % 5) * 0.5f) , transform.position.y - 0.3f + ((i % 3) * 0.5f), transform.position.z));
+			birds [i].GetComponent<BirdBehavior> ().setRowAndColumn (i % 3, i % 5);
+			birds [i].SetActive (false);
+
+		}
+	}
+	
+	// Update is called once per physics update
+	void FixedUpdate () {
+
+		float pivotRot = sunPivotObject.transform.rotation.eulerAngles.z;
+		if (current_range == -1) {
+			
+			for (int i = 0; i < ranges.Length; i++) {
+				if (ranges [i].inRange (pivotRot)) {
+					current_range = i;
+					for (int j = 0; j < 5; j++) {
+						birds [indices [current_range, j]].SetActive (true);
+						birds [indices [current_range, j]].GetComponent<BirdBehavior>().setToFencePosition ();
+					}
+					break;
+				}
+			}
+		} else {
+			if (!ranges [current_range].inRange (pivotRot)) {
+				for (int i = 0; i < 5; i++) {
+					birds [indices [current_range, i]].SetActive (false);
+				}
+				current_range = -1;
+
+			}
+
+		}
+	}
+}
