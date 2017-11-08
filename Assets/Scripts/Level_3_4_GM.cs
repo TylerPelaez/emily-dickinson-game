cce8ef41e26ca018260d62857459808362b5582d
anniepa <anniepa246@gmail.com>
2017-11-08 00:40:24 -0500

    level 3 4 skeleton done

--- Assets/Scripts/Level_3_4_GM.cs
+++ Assets/Scripts/Level_3_4_GM.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Level_3_4_GM : MonoBehaviour {
-	public GameObject[] clouds;
+	public GameObject cloud;
 	public GameObject sun;
 	public GameObject key;
 
+	public GameObject photo;
+	public GameObject cup;
 
 	bool level3_state;
 	bool cloud_state;
 	bool sun_state;
-	bool rain_state;
-	bool key_state; //transitions to level 4
 
 	bool level4_state;
 	bool photo_state;
@@ -24,8 +24,6 @@ public class Level_3_4_GM : MonoBehaviour {
 		level3_state = false;
 		cloud_state = false;
 		sun_state = false;
-		rain_state = false;
-		key_state = false;
 
 		level4_state = false;
 		photo_state = false;
@@ -34,6 +32,56 @@ public class Level_3_4_GM : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(!level3_state){
+			check_level3();
+		}else{
+			if(!level4_state){
+				check_level4();
+			}else{
+				Debug.Log("done");
+			}
+		}
+	}
+
+	void check_sun(){
+		if(sun.transform.eulerAngles.z < 180f && sun.transform.eulerAngles.z > 0f){
+			sun_state = true;
+		}
+	}
+
+	void check_cloud(){
+		if(cloud.transform.eulerAngles.y < 180f && cloud.transform.eulerAngles.y > 0f){
+			cloud_state = true;
+		}
+	}
+
+	void check_level3(){
+		if(!sun_state){
+			check_sun();
+		}
+		if(!cloud_state){
+			check_cloud();
+		}
+
+		if(sun_state && cloud_state){
+			level3_state = true;
+		}
+	}
+
+	void check_photo(){
 		
+	}
+
+	void check_cup(){
+		
+	}
+
+	void check_level4(){
+		if(!photo_state){
+			check_photo();
+		}
+		if(!cup_state){
+			check_cup();
+		}
 	}
 }
