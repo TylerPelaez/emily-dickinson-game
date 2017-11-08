e05d6917b6caefff85da2ebd7586a12c7ef64215
anniepa <anniepa246@gmail.com>
2017-11-08 05:35:05 -0500

    level 1 started

---
+++ Assets/Scripts/Level_1_GM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1_GM : MonoBehaviour {
+	public Playback sun;
+	public GameObject player;
+
+	bool turning;
+	// Use this for initialization
+	void Start () {
+		turning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		check_turning();
+	}
+
+	void check_turning(){
+		int dir = player.GetComponent<PlayerCam>().turn;
+		if(dir == 0){
+			turning = true;
+		}else{
+			turning = false;
+		}
+	}
+}
